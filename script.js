d6c07adda88ae612204a5955ae665abb1ff9f978
FraualphaMar <257962461+FraualphaMar@users.noreply.github.com>
2026-04-01 16:29:01 +0800

    Update sketch color with random colors

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`;
+}
 const container = document.querySelector(".container");
 const gridSize = 16 * 16;
 for (let i = 1; i <= gridSize; i++) {
     const div = document.createElement("div");
     div.addEventListener("mouseenter", (event) => {
-        event.target.style.backgroundColor = "orange";
+        event.target.style.backgroundColor = getRandomColor();
     });
     container.appendChild(div);
 }
@@ -21,10 +27,10 @@ button.addEventListener("click", () => {
     const squareSize = 480 / input;
     for (let i = 0; i < gridSize; i++) {
         const div = document.createElement("div");
-        div.style.width = squareSize + "px";
-        div.style.height = squareSize + "px";
+        div.style.width = `${squareSize}px`;
+        div.style.height = `${squareSize}px`;
         div.addEventListener("mouseenter", (event) => {
-            event.target.style.backgroundColor = "orange";
+            event.target.style.backgroundColor = getRandomColor();
         })
         container.appendChild(div);
     }
