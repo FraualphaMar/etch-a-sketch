a8a2d6e984149e6a95d6f65c20c2ebde8d9c082c
FraualphaMar <257962461+FraualphaMar@users.noreply.github.com>
2026-04-01 15:47:38 +0800

    Add html, css and js files

---
+++ script.js
@@ -0,0 +1,31 @@
+const container = document.querySelector(".container");
+const gridSize = 16 * 16;
+for (let i = 1; i <= gridSize; i++) {
+    const div = document.createElement("div");
+    div.addEventListener("mouseenter", (event) => {
+        event.target.style.backgroundColor = "orange";
+    });
+    container.appendChild(div);
+}
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    const input = prompt("how many squares per side for your new grid?(Max 100)")
+    if (input === null) return;
+    if (input < 1 || input > 100) {
+        alert("Please enter a number between 1 and 100");
+        return;
+    }
+    container.innerHTML = "";
+    const gridSize = input * input;
+    const squareSize = 480 / input;
+    for (let i = 0; i < gridSize; i++) {
+        const div = document.createElement("div");
+        div.style.width = squareSize + "px";
+        div.style.height = squareSize + "px";
+        div.addEventListener("mouseenter", (event) => {
+            event.target.style.backgroundColor = "orange";
+        })
+        container.appendChild(div);
+    }
+})
